5a9517510ce40a122e877d4767cfd5995e1eaede
Boris Yakimov <boris.yakimov@itgix.com>
2024-06-19 18:04:25 +0300

    #29 move aws ram module into separate repo

--- ram/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "resource_association" {
-  description = "Object with the AWS RAM resource association resource"
-  value       = aws_ram_resource_association.this
-}
-
-output "principal_association" {
-  description = "Object with the AWS RAM principal association resource"
-  value       = aws_ram_principal_association.this
-}
